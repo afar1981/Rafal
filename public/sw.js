fd037424886bd81a82b13daad7330f3013e41f6f
afar1981 <90156577+afar1981@users.noreply.github.com>
2026-09-17 21:01:54 +0200

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = 'polska-tradycja-v1';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
