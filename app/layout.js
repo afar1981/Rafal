1fa7f63217b6c1d119fb8ac7349f9b48e47730d5
afar1981 <90156577+afar1981@users.noreply.github.com>
2026-09-17 20:09:38 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,3 @@
 import './globals.css'
-export const metadata={title:'POLSKA TRADYCJA – ZAMÓWIENIA',description:'Internetowe zamówienia POLSKA TRADYCJA'}
+export const metadata={title:'POLSKA TRADYCJA – ZAMÓWIENIA',description:'Internetowe zamówienia POLSKA TRADYCJA',manifest:'/manifest.json'}
 export default function RootLayout({children}){return <html lang="pl"><body>{children}</body></html>}
